39054ecf3d26c314c33f15fdc19ed82242810783
Tarun Vishwakarma <tarunvishwakarma81@gmail.com>
2026-09-07 22:21:02 +0530

    fix: env local dev file changes

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,31 @@
 mod config;
 
 use anyhow::Error;
+use tokio_postgres::NoTls;
 
-fn main() -> Result<(), Error> {
+#[tokio::main]
+async fn main() -> Result<(), Error> {
     dotenvy::dotenv().ok();
 
     let config = config::env::Env::load()?;
-
     println!("config: {}", config);
+
+    let postgres = tokio_postgres::connect(config.db_url.as_str(), NoTls).await?;
+
+    tokio::spawn(async move {
+        if let Err(e) = postgres.1.await {
+            eprintln!("connection error: {}", e)
+        }
+    });
+
+    let rows = postgres
+        .0
+        .query("SELECT $1::TEXT", &[&"hello world"])
+        .await?;
+
+    let value: &str = rows[0].get(0);
+
+    println!("{}", value);
 
     Ok(())
 }
